613c7d02b0f67feec6d61c10eaf1c70727a68b8e
PlagueWZK <2849996270@qq.com>
2024-04-14 18:28:28 +0800

    The First Version

--- src/wzk/newMath/MainBody.java
+++ src/wzk/newMath/MainBody.java
@@ -3,6 +3,7 @@ package wzk.newMath;
 
 public class MainBody {
     public static void main(String[] args) {
+        /*
         System.out.println(new Fraction(1,2).add(new Fraction(2,1)));
         System.out.println(new Fraction(3,2));
         System.out.println(Fraction.fractionFormatter(3,2));
@@ -10,6 +11,8 @@ public class MainBody {
         System.out.println(det);
         System.out.println(det.getResult());
         System.out.println(det.getDoubleResult());
+        */
+        //Up is test code!!
 
     }
 }
